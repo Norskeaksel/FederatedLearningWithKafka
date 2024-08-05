7ba391a64878fd75bceb7aae66927c1b20edf4a3
Aksel <ahsorbye@gmail.com>
2024-08-05 10:15:08 +0200

    Add git clone step to readme

--- controller.py
+++ controller.py
@@ -1,10 +1,9 @@
-import uuid
 from kafkaLogic.consumeTrainAndProduce import consumeTrainAndProduce
 from kafkaLogic.read_config import read_config
 
 
 config = read_config()
-consumerGroup = "Aksel's controller v1"
+consumerGroup = "Global controller"
 consumer_topic = "client_weights_v1"
 producer_topic = "aggregated_weights_v1"
 
